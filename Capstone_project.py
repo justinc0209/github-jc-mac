3ad5b65355e71e29d0f8ff18af0b9775aec43ec3
Justin Chai <Chai@Justins-MBP.broadband>
2019-09-28 22:27:33 +0100

    file created for the captone project

---
+++ Capstone_project.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# This Notebook is being used for the Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
